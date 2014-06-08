1cba3d2ae723cdf2bef03d0702f42737766fd287
junkls <junkls000@gmail.com>
2014-06-08 23:01:58 +0900

    update

---
+++ 1_make_window.py
@@ -0,0 +1,28 @@
+# ウインドウを出すだけ
+
+# sdl2.ext ... SDL2のpython extentions pysdl2の基本となる
+import sdl2.ext 
+import time
+
+# 640x400のウインドウを作る
+window = sdl2.ext.Window("タイトル", size=(640, 400))
+
+# 640x400のウインドウを表示
+window.show()
+
+# 1秒待つ
+time.sleep(1)
+
+# 最大化して表示
+#window.maximize()
+
+# 最小化して表示
+#window.minimize()
+
+# ウインドウを隠す
+window.hide()
+
+# 1秒待って表示
+time.sleep(1)
+window.show()
+time.sleep(1)
---
+++ 2_show_image.py
@@ -0,0 +1,41 @@
+"""
+画像を表示させる
+
+流れ
+1. windowをつくる
+2. factoryをつくる
+3. factory.from_image("画像")からspriteをつくる
+4. factory(window)からSpriteRenderSystemをつくる
+5. render(sprite)で描画
+"""
+
+import sdl2.ext
+import time
+
+# 初期化
+sdl2.ext.init()
+
+# windowをつくる
+window = sdl2.ext.Window("タイトル", size=(800, 600))
+window.show()
+
+# spriteオブジェクトを作るfactory
+# factoryからspriteオブジェクト(2_image.jpg)を作る
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+sprite = factory.from_image("2_image.jpg")
+
+# factoryから新しいSpriteRenderSystemをつくる
+# SpriteRenderSystemはspriteオブジェクトのためのレンダリングシステム
+# renderメソッドでspriteオブジェクトを描画する
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(sprite)
+
+# 2秒待つ
+time.sleep(2)
+
+# spriteオブジェクトの描画位置の変更
+sprite.position = 100, 100
+spriterenderer.render(sprite)
+
+# 2秒待つ
+time.sleep(2)
